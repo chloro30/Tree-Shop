1d8e69c83e779847383660e6a4c2a98f719ec0ff
chloro30 <chloro30@gmail.com>
2022-02-17 16:05:35 +0900

    db 연결 정보 수정

--- db_test.php
+++ db_test.php
@@ -1,9 +1,13 @@
 <?php
-$db_host = "localhost";
-$db_user = "DB_아이디";
-$db_passwd = "DB_비밀번호";
-$db_name = "DB_명"; 
+// $db_host = "localhost";
+// $db_user = "DB_아이디";
+// $db_passwd = "DB_비밀번호";
+// $db_name = "DB_명"; 
 
+$db_host = "localhost";
+$db_user = "root";
+$db_passwd = "1234";
+$db_name = "treeshop"; 
 $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
 
 if (mysqli_connect_errno($conn)) {
