23c0c010da23661c5a967bf57449465eaee53de2
chloro30 <chloro30@gmail.com>
2022-02-20 09:23:44 +0900

    footer 및 가격 자리수 수정

--- index.php
+++ index.php
@@ -175,8 +175,8 @@
                 <div>
                     <p>
                         (주)Tree_shop<br/>
-                        대표이사 : 홍길동, 주소: 서울특별시 종로구<br/>
-                        사업자등록번호 : 512-12-12345, 통신판매업신고 : 1234-서울종로구-1234
+                        대표이사 : 홍길동&nbsp;&nbsp;주소: 서울특별시 종로구<br/>
+                        사업자등록번호 : 512-12-12345&nbsp;&nbsp;통신판매업신고 : 1234-서울종로구-1234
                     </p>
                     <p>
                         고객센터 1234-1234<br/>
@@ -184,7 +184,7 @@
                         Fax : 02-4321-2345 / E-mail : customerservice@treeshop.com
                     </p>
                 </div>
-                <address>Copyright ⓒ 2021 All rights reserved.</address>
+                <address>Copyright ⓒ 2022 All rights reserved.</address>
             </div>
         </footer>
     </div>
--- web/login/login.php
+++ web/login/login.php
@@ -73,7 +73,7 @@
             const loginForm = document.querySelector('#loginForm');
             const loginBtn = document.querySelector('#loginBtn');
             const userId = document.querySelector('#userId');
-            const userPw =document.querySelector('#userPw');
+            const userPw = document.querySelector('#userPw');
 
             loginBtn.addEventListener('click',function(){
                 if(userId.value!="" && userPw.value!=""){
@@ -93,8 +93,8 @@
                 <div>
                     <p>
                         (주)Tree_shop<br/>
-                        대표이사 : 홍길동, 주소: 서울특별시 종로구<br/>
-                        사업자등록번호 : 512-12-12345, 통신판매업신고 : 1234-서울종로구-1234
+                        대표이사 : 홍길동&nbsp;&nbsp;주소: 서울특별시 종로구<br/>
+                        사업자등록번호 : 512-12-12345&nbsp;&nbsp;통신판매업신고 : 1234-서울종로구-1234
                     </p>
                     <p>
                         고객센터 1234-1234<br/>
@@ -102,7 +102,7 @@
                         Fax : 02-4321-2345 / E-mail : customerservice@treeshop.com
                     </p>
                 </div>
-                <address>Copyright ⓒ 2021 All rights reserved.</address>
+                <address>Copyright ⓒ 2022 All rights reserved.</address>
             </div>
         </footer>
     </div>
